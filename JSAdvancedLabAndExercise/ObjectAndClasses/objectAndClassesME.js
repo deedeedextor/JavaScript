fbacb0ce081a59fc41f190d035e5f5c23e5b30a8
deedeedextor <zavinagi_15@abv.bg>
2020-11-19 14:50:11 +0200

    Add exercise for class

--- JSAdvancedLabAndExercise/ObjectAndClasses/objectAndClassesME.js
+++ JSAdvancedLabAndExercise/ObjectAndClasses/objectAndClassesME.js
@@ -177,7 +177,61 @@ uniqueSequences(["[7.14, 7.180, 7.339, 80.099]",
 "[7.339, 80.0990, 7.140000, 7.18]", 
 "[7.339, 7.180, 7.14, 80.099]"]);
 
+let result = (function(){
+    let validFaces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+    let validSuits = ['♠', '♥', '♦', '♣'];
+    let Suits = {
+        SPADES: '♠',
+        HEARTS: '♥',
+        DIAMONDS: '♦',
+        CLUBS: '♣',
+    };
+ 
+    class Card {
+        constructor(face, suit) {
+            this.face = face;
+            this.suit = suit;
+        }
+ 
+        get face() {
+            return this._face;
+        }
+ 
+        set face(face) {
+            if (!validFaces.includes(face)) {
+                throw new Error('Invalid Card Face!');
+            }
+            this._face = face;
+            return;
+        }
+ 
+        get suit() {
+            return this._suit;
+        }
+ 
+        set suit(suit) {
+            if (!validSuits.includes(suit)) {
+                throw new Error('Invalid Card Suit!');
+            }
+            this._suit = suit;
+            return;
+        }
+    }
+ 
+    return {
+        Suits: Suits,
+        Card: Card
+    }
+}());
+/*
+let Card = result.Card;
+let Suits = result.Suits;
 
+let card = new Card('Q', Suits.CLUBS);
+card.face = 'A';
+card.suit = Suits.DIAMONDS;
+let card2 = new Card('1', Suits.DIAMONDS);
+*/
 
 
 
