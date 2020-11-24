48a24f56b0d609231449b57c364464f02d6c770e
deedeedextor <zavinagi_15@abv.bg>
2020-11-24 16:03:10 +0200

    Add Format the text exercise - DOM

--- JSAdvancedLabAndExercise/DOM/Lab/05. JS-Advanced-DOM-Lab-Resources/02. Format the Text/solution.js
+++ JSAdvancedLabAndExercise/DOM/Lab/05. JS-Advanced-DOM-Lab-Resources/02. Format the Text/solution.js
@@ -1,3 +1,17 @@
 function solve() {
-  //TODO...
+  const inputText = document.getElementById('input').innerText;
+  const outputElement = document.getElementById('output');
+
+  inputText
+    .match(/(?:\s*)([^.!?]+[.!?]+)/g)
+    .map(sentence => sentence.trim())
+    .reduce((acc, sentence, index) => {
+      if (index % 3 === 0) { acc.push([sentence]); } else { acc[acc.length - 1].push(sentence); }
+      return acc;
+    }, [])
+    .forEach(paragraph => {
+      let pHTML = document.createElement('p');
+      pHTML.textContent = paragraph;
+      outputElement.appendChild(pHTML);
+    });
 }
