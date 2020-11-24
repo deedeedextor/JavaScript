5b44a80d116268d8774d4ea15a63c9bb85622934
deedeedextor <zavinagi_15@abv.bg>
2020-11-24 15:37:11 +0200

    Add article list exercise - DOM

--- JSAdvancedLabAndExercise/DOM/Lab/05. JS-Advanced-DOM-Lab-Resources/01. Articles List/solution.js
+++ JSAdvancedLabAndExercise/DOM/Lab/05. JS-Advanced-DOM-Lab-Resources/01. Articles List/solution.js
@@ -1,3 +1,26 @@
 function createArticle() {
 	
+    let titleElement = document
+    .getElementById("createTitle");
+    let contentElement = document
+    .getElementById("createContent");
+    let articles = document
+    .getElementById("articles");
+
+    let article = document
+    .createElement('article');
+    let h3 = document.createElement('h3');
+    let p = document.createElement('p');
+
+    if (titleElement.value !== '' && contentElement.value !== '') {
+        h3.innerHTML = titleElement.value;
+        titleElement.value = '';
+        p.innerHTML = contentElement.value;
+        contentElement.value = '';
+
+        article.appendChild(h3);
+        article.appendChild(p);
+
+        articles.appendChild(article);
+    }
 }
