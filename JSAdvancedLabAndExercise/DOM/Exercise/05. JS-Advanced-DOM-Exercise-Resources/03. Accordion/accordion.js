10ae2371a8e40a1f8b8843a2455e0ee77e4602a5
deedeedextor <zavinagi_15@abv.bg>
2020-11-25 16:52:21 +0200

    Add exercise Accordion - DOM

--- JSAdvancedLabAndExercise/DOM/Exercise/05. JS-Advanced-DOM-Exercise-Resources/03. Accordion/accordion.js
+++ JSAdvancedLabAndExercise/DOM/Exercise/05. JS-Advanced-DOM-Exercise-Resources/03. Accordion/accordion.js
@@ -1,3 +1,15 @@
 function toggle() {
-    console.log('TODO:...');
+    let buttonElement = document
+    .getElementsByClassName('button')[0];
+    let pElement = document.getElementById('extra');
+    
+    if (buttonElement.innerHTML == 'More') {
+        buttonElement.innerHTML = "Less"
+
+        pElement.style.display = 'block';
+    }
+    else{
+        buttonElement.innerHTML = 'More'
+        pElement.style.display = 'none';
+    }
 }
