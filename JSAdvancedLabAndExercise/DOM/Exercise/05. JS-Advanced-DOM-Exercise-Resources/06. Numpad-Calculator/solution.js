52443d3368eeb7cb6462eaf2712bdb1455b06c57
deedeedextor <zavinagi_15@abv.bg>
2020-11-26 15:17:55 +0200

    Add calculator exercise - DOM

--- JSAdvancedLabAndExercise/DOM/Exercise/05. JS-Advanced-DOM-Exercise-Resources/06. Numpad-Calculator/solution.js
+++ JSAdvancedLabAndExercise/DOM/Exercise/05. JS-Advanced-DOM-Exercise-Resources/06. Numpad-Calculator/solution.js
@@ -1,4 +1,50 @@
 function solve() {
+    let keys = Array
+    .from(document.getElementsByClassName('keys'));
+    let expressionOutput = document
+    .getElementById('expressionOutput');
+    let result = document
+    .getElementById('result');
+    let clearButton = document
+    .querySelector('.clear');
 
-    //TODO...
+    let operand, firstNumber = '', secondNumber = '';
+
+    const calculator = {
+        '+': (a, b) => a + b,
+        '-': (a, b) => a - b,
+        '*': (a, b) => a * b,
+        '/': (a, b) => a / b
+    }
+
+    keys.map(key => key.addEventListener('click', function(e){
+        let currentSelection = e.target.value;
+
+        clearButton.addEventListener('click', function(){
+            expressionOutput.innerHTML = '';
+            result.innerHTML = '';
+            firstNumber = '';
+            secondNumber = '';
+            operand = '';
+        })
+
+        if (+currentSelection || currentSelection == '.' || currentSelection == '0') {
+            if (!operand) {
+                firstNumber += currentSelection;
+                expressionOutput.innerHTML += currentSelection;
+            } else {
+                secondNumber += currentSelection;
+                expressionOutput.innerHTML += currentSelection
+            }
+        } else if (calculator.hasOwnProperty(currentSelection)) {
+            operand = currentSelection;
+            expressionOutput.innerHTML += ` ${operand} `;
+        } else if (currentSelection == '=') {
+            if (+firstNumber && +secondNumber) {
+                result.innerHTML = calculator[operand](+firstNumber, +secondNumber);
+            } else {
+                result.innerHTML = 'NaN';
+            }
+        }
+    }))
 }
