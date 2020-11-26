adb8193635c6667578cc5616e285a0d65195d5e3
deedeedextor <zavinagi_15@abv.bg>
2020-11-26 16:08:24 +0200

    Add NumberConverter exercise - DOM

--- JSAdvancedLabAndExercise/DOM/Exercise/05. JS-Advanced-DOM-Exercise-Resources/07. Number-Convertor/solution.js
+++ JSAdvancedLabAndExercise/DOM/Exercise/05. JS-Advanced-DOM-Exercise-Resources/07. Number-Convertor/solution.js
@@ -1,4 +1,28 @@
 function solve() {
+    let selectTo = document.getElementById('selectMenuTo');
+    let binary = document.createElement('option');
+    binary.innerHTML = 'Binary';
+    binary.value = 'binary';
+    let hexadecimal = document.createElement('option');
+    hexadecimal.innerHTML = 'Hexadecimal';
+    hexadecimal.value = 'hexadecimal';
 
-    //TODO...
+    selectTo.appendChild(binary);
+    selectTo.appendChild(hexadecimal);
+
+    let input = document
+    .getElementById('input');
+    let button = document
+        .getElementsByTagName('button')[0];
+    let result = document
+        .getElementById('result');
+
+    const selectMap = {
+        'binary' : num => num.toString(2),
+        'hexadecimal' : num => num.toString(16).toUpperCase()
+    }
+
+    button.addEventListener('click', function(){
+        result.value = selectMap[selectTo.value](+input.value);
+    })
 }
