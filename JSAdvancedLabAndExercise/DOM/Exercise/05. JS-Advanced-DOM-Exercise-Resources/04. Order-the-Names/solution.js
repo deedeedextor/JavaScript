2f2546cc26364ffc18e5713a72c3ac19a87d1890
deedeedextor <zavinagi_15@abv.bg>
2020-11-26 14:18:04 +0200

    Add Order the names exercise - DOM

--- JSAdvancedLabAndExercise/DOM/Exercise/05. JS-Advanced-DOM-Exercise-Resources/04. Order-the-Names/solution.js
+++ JSAdvancedLabAndExercise/DOM/Exercise/05. JS-Advanced-DOM-Exercise-Resources/04. Order-the-Names/solution.js
@@ -1,4 +1,26 @@
 function solve() {
+    let input = document.getElementsByTagName('input')[0];
+    let button = document.getElementsByTagName('button')[0];
+    let firstLetter = '';
 
-    //TODO...
+    button.addEventListener('click', function(){
+        let name = input.value;
+
+        firstLetter = name[0].toUpperCase();
+        let index = firstLetter.charCodeAt(0) - 65;
+
+        let orderedList = document.querySelectorAll('ol li');
+        if (orderedList[index].innerHTML === '') {
+            orderedList[index].innerHTML += firstLetter + 
+            name.slice(1).toLowerCase();
+        } else {
+            orderedList[index].innerHTML += ", " + 
+            firstLetter + 
+            name.slice(1).toLowerCase();
+        }
+
+        input.value = '';
+    })
+
+
 }
