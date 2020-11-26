d77008e15587588bd6afc2f387686c40b25ae809
deedeedextor <zavinagi_15@abv.bg>
2020-11-26 14:51:48 +0200

    Add ChatRoom exercise - DOM

--- JSAdvancedLabAndExercise/DOM/Exercise/05. JS-Advanced-DOM-Exercise-Resources/05. Chat-Room/solution.js
+++ JSAdvancedLabAndExercise/DOM/Exercise/05. JS-Advanced-DOM-Exercise-Resources/05. Chat-Room/solution.js
@@ -1,5 +1,16 @@
 function solve() {
-   //TODO...
+  let chatRoom = document.getElementById("chat_messages");
+  let input = document.getElementById("chat_input");
+  let button = document.getElementById("send");
+
+  button.addEventListener('click', function () {
+    let myMessage = document.createElement('div');
+    myMessage.classList.add('message');
+    myMessage.classList.add('my-message');
+
+    myMessage.innerHTML = input.value;
+    chatRoom.appendChild(myMessage);
+
+    input.value = '';
+  });
 }
-
-
