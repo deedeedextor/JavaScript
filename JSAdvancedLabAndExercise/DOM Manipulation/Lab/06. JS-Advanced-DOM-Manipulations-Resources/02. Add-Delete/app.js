0a03291e1bb7bb2843f7177da79dba8f795ef534
deedeedextor <zavinagi_15@abv.bg>
2020-12-03 11:53:05 +0200

    Add task Add/Delete - DOM Manipulations

--- JSAdvancedLabAndExercise/DOM Manipulation/Lab/06. JS-Advanced-DOM-Manipulations-Resources/02. Add-Delete/app.js
+++ JSAdvancedLabAndExercise/DOM Manipulation/Lab/06. JS-Advanced-DOM-Manipulations-Resources/02. Add-Delete/app.js
@@ -1,3 +1,28 @@
 function addItem() {
-    //TODO...
+    let button = document
+    .getElementsByTagName('input')[1];
+
+
+    button.addEventListener('click', () => {
+    let inputText = document
+    .getElementById('newText');
+    let listItems = document.getElementById('items');
+
+    if (inputText.value !== '') {
+        let newItem = document.createElement('li');
+        newItem.innerHTML = inputText.value;
+        let deleteElement = document.createElement('span');
+        deleteElement.innerHTML = '          [Delete]';
+        deleteElement.style.cursor = 'pointer';
+        newItem.appendChild(deleteElement);
+
+        deleteElement.addEventListener('click', (e) => {
+            let parentElement = e.target.parentElement;
+            parentElement.remove();
+
+        })
+        listItems.appendChild(newItem);
+        inputText.value = '';
+    }
+})
 }
