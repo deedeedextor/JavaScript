102ccb305429c4b6b7b895f652df91ccdc51170c
deedeedextor <zavinagi_15@abv.bg>
2020-12-03 12:18:55 +0200

    Add Delete from Table - DOM Manipulations

--- JSAdvancedLabAndExercise/DOM Manipulation/Lab/06. JS-Advanced-DOM-Manipulations-Resources/03. Delete-From-Table/app.js
+++ JSAdvancedLabAndExercise/DOM Manipulation/Lab/06. JS-Advanced-DOM-Manipulations-Resources/03. Delete-From-Table/app.js
@@ -1,3 +1,19 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+  let emails = document
+  .querySelectorAll("table tr td:nth-child(2)");
+  let deleted = false;
+
+  for (let email of emails) {
+    if (email.textContent == document
+        .getElementsByName("email")[0].value) {
+        email.parentNode.parentNode
+        .removeChild(email.parentNode);
+
+        deleted = true;
+    }
+  }
+
+  document.getElementById("result").textContent = deleted
+    ? "Deleted."
+    : "Not found.";
 }
