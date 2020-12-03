228bee451188721850bb0d5a5856007f97b7e23d
deedeedextor <zavinagi_15@abv.bg>
2020-12-03 11:33:42 +0200

    Add task List of items - DOM Manipulations

--- JSAdvancedLabAndExercise/DOM Manipulation/Lab/06. JS-Advanced-DOM-Manipulations-Resources/01. List-Of-Items/app.js
+++ JSAdvancedLabAndExercise/DOM Manipulation/Lab/06. JS-Advanced-DOM-Manipulations-Resources/01. List-Of-Items/app.js
@@ -1,3 +1,17 @@
 function addItem() {
-    console.log('TODO:...');
+    let button = document.getElementsByTagName('input')[1];
+
+
+    button.addEventListener('click', () => {
+        let inputText = document
+        .getElementById('newItemText');
+        let listItems = document.getElementById('items');
+
+        if (inputText.value !== '') {
+            let newItem = document.createElement('li');
+            newItem.innerHTML = inputText.value;
+            listItems.appendChild(newItem);
+            inputText.value = '';
+        }
+    })
 }
