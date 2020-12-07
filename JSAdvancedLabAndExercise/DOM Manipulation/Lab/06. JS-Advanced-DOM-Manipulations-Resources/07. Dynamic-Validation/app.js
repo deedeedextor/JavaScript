25a73d93d97b73d22da325048db9af13f14ccd49
deedeedextor <zavinagi_15@abv.bg>
2020-12-07 16:07:45 +0200

    Add Dynamic Validation - DOM Manipulations

--- JSAdvancedLabAndExercise/DOM Manipulation/Lab/06. JS-Advanced-DOM-Manipulations-Resources/07. Dynamic-Validation/app.js
+++ JSAdvancedLabAndExercise/DOM Manipulation/Lab/06. JS-Advanced-DOM-Manipulations-Resources/07. Dynamic-Validation/app.js
@@ -1,3 +1,15 @@
 function validate() {
-    console.log('TODO:...');
+  let reg = /^([\w\-.]+)@([a-z]+)(\.[a-z]+)+$/;
+  let inputElement = document.getElementById("email");
+
+  inputElement.addEventListener("change", checkEmail);
+
+  function checkEmail(e) {
+    if (reg.test(e.target.value)) {
+      e.target.removeAttribute("class");
+      return;
+    }
+
+    e.target.className = "error";
+  }
 }
