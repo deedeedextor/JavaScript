49aa410e176b90ceb590f7dcf8d93e7495a26d08
deedeedextor <zavinagi_15@abv.bg>
2020-12-07 12:20:03 +0200

    Add Highlight Active - DOM Manipulation

--- JSAdvancedLabAndExercise/DOM Manipulation/Lab/06. JS-Advanced-DOM-Manipulations-Resources/06. Highlight-Active/app.js
+++ JSAdvancedLabAndExercise/DOM Manipulation/Lab/06. JS-Advanced-DOM-Manipulations-Resources/06. Highlight-Active/app.js
@@ -1,3 +1,19 @@
 function focus() {
-    console.log('TODO:...');
+    let mainDiv = document.getElementsByTagName("div")[0];
+
+    Array.from(mainDiv.getElementsByTagName("input")).forEach(element => {
+        element.addEventListener("focus",focus);
+        element.addEventListener("blur",focusLost);
+    });
+    
+    function focus(e) {
+        let parent = e.target.parentNode;
+       parent.classList.add("focused");
+    }
+
+
+    function focusLost(e) {
+        let parent = e.target.parentNode;
+       parent.classList.remove("focused");
+    }
 }
