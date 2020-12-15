fbf247b0a778c7a8130a57d30dd12cb31ebb4262
deedeedextor <zavinagi_15@abv.bg>
2020-12-15 14:59:07 +0200

    Add exercise Locked Profile - DOM Manipulation

--- JSAdvancedLabAndExercise/DOM Manipulation/Exercise/06. JS-Advanced-DOM-Manipulations-Exercise-Resources/04. Locked-Profile/app.js
+++ JSAdvancedLabAndExercise/DOM Manipulation/Exercise/06. JS-Advanced-DOM-Manipulations-Exercise-Resources/04. Locked-Profile/app.js
@@ -1,3 +1,21 @@
 function lockedProfile() {
-    console.log('TODO...')
+    [...document.getElementsByClassName('profile')].forEach((parent) => {
+           let btn = parent.querySelectorAll('button')[0];
+
+           btn.addEventListener('click', () => {
+               let unlock = parent.querySelectorAll('input')[1].checked;
+               let hiddenDiv = parent.querySelectorAll('div')[0];
+
+               if (unlock) {
+                if (btn.textContent == 'Show more') {
+                    hiddenDiv.style.display = 'block';
+                    btn.textContent = 'Hide it';
+                } else {
+                 hiddenDiv.style.display = 'none';
+                 btn.textContent = 'Show more';
+                }
+               }
+           })
+        }
+    )
 }
