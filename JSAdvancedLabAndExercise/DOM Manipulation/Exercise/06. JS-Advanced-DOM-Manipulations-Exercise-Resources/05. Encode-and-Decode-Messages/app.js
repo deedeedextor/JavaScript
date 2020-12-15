b2828c58cf176a38508435c27b4542a1412aa1ec
deedeedextor <zavinagi_15@abv.bg>
2020-12-15 15:43:45 +0200

    Add exercise Encode and Decode messages - DOM Manipulations

--- JSAdvancedLabAndExercise/DOM Manipulation/Exercise/06. JS-Advanced-DOM-Manipulations-Exercise-Resources/05. Encode-and-Decode-Messages/app.js
+++ JSAdvancedLabAndExercise/DOM Manipulation/Exercise/06. JS-Advanced-DOM-Manipulations-Exercise-Resources/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,35 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let encodeBtn = document
+    .querySelectorAll('button')[0];
+    let decodeBtn = document
+    .querySelectorAll('button')[1];
+    let messageInput = document
+    .querySelectorAll('textarea')[0];
+    let messageOutput = document
+    .querySelectorAll('textarea')[1];
+
+    encodeBtn.addEventListener('click', () => {
+        let message = messageInput.value;
+        let encodedMessage = '';
+        for (let i = 0; i < message.length; i++) {
+            encodedMessage += String
+            .fromCharCode(ascii(`${message[i]}`) + 1);
+        }
+        messageInput.value = '';
+        messageOutput.value = encodedMessage;
+    });
+
+    decodeBtn.addEventListener('click', () => {
+        let message = messageOutput.value;
+        let decodedMessage = '';
+        for (let i = 0; i < message.length; i++) {
+            decodedMessage += String
+            .fromCharCode(ascii(`${message[i]}`) - 1);
+        }
+        messageOutput.value = decodedMessage;
+    });
+    
+    function ascii(a) {
+        return a.charCodeAt(0);
+    }
 }
