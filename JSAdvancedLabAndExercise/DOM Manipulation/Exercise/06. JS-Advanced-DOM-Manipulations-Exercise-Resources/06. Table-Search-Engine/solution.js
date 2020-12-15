9c834f2b9c672fdcb911aa2979993d6189abc253
deedeedextor <zavinagi_15@abv.bg>
2020-12-15 16:06:44 +0200

    Add exercise Table search - DOM Manipulations

--- JSAdvancedLabAndExercise/DOM Manipulation/Exercise/06. JS-Advanced-DOM-Manipulations-Exercise-Resources/06. Table-Search-Engine/solution.js
+++ JSAdvancedLabAndExercise/DOM Manipulation/Exercise/06. JS-Advanced-DOM-Manipulations-Exercise-Resources/06. Table-Search-Engine/solution.js
@@ -1,3 +1,20 @@
 function solve() {
-   //TODO...
+  let dataTr = Array.from(document.querySelectorAll("tbody > tr"));
+  let searchField = document.querySelector("#searchField");
+  let searchBtn = document.querySelector("#searchBtn");
+
+  searchBtn.addEventListener("click", () => {
+    let regex = new RegExp(searchField.value, "gim");
+
+    if (searchField.value !== "") {
+      dataTr.map((e) => {
+        e.classList.remove("select");
+
+        if (e.innerHTML.match(regex) !== null) {
+          e.className = 'select';
+        }
+      });
+      searchField.value = '';
+    }
+  });
 }
